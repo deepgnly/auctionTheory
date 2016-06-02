a2cf4cce82bb57c2d44a2a1ffd9b723cd6aca2ce
i072231 <soumyadeep.ganguly@sap.com>
2016-06-02 19:49:51 +0530

    market profile calculation added on the heap

--- auctionTheory/StoreTickData.py
+++ auctionTheory/StoreTickData.py
@@ -1,18 +1,84 @@
 class StoreTickData(object):
 
     scripName=None
-    priceToAlphabetDict={}
+    priceToAlphabetsDict={}
+    timeFrameToAlphabetList={}
+    startTime=None
+    endTime=None
+    profileAlphabetsArray=None
+    timeFrame=None
+    doStoreOnlyInHeap=None
 
-    def __init__(self,scripName):
+    def __init__(self,scripName,marketStartTime,marketEndTime,profileAlphabetsArray,timeFrame,doStoreOnlyInHeap):
+
         self.scripName=scripName
+        self.startTime=marketStartTime
+        self.endTime=marketEndTime
+        self.profileAlphabetsArray=profileAlphabetsArray
+        self.timeFrame=timeFrame
+        self.doStoreOnlyInHeap=doStoreOnlyInHeap
+        self._mapTimePeriodsToAlphabets(self.timeFrame,self.profileAlphabetsArray,self.startTime,self.endTime)
 
-    def isAlphabetAlreadyIncludedInTheTick(self,price,timeframeAlphabet):
-        if price not in self.priceToAlphabetDict:
-            self.priceToAlphabetDict[price]=[timeframeAlphabet]
+
+    def _isAlphabetAlreadyIncludedInTheTick(self,price,timeframeAlphabet):
+
+        if price not in self.priceToAlphabetsDict:
+            self.priceToAlphabetsDict[price]=[timeframeAlphabet]
+            return False
         else:
-            timeframeAlphabetList=self.priceToAlphabetDict[price]
-            if timeframeAlphabetList.count>0:
-                timeframeAlphabetList.append(timeframeAlphabet)
+            timeframeAlphabetsList=self.priceToAlphabetsDict[price]
+
+            if timeframeAlphabet in timeframeAlphabetsList:
+                return True
             else:
-                timeframeAlphabetList=[timeframeAlphabet]
-            self.priceToAlphabetDict[price]=timeframeAlphabetList
+                timeframeAlphabetsList.append(timeframeAlphabet)
+                self.priceToAlphabetsDict[price] = timeframeAlphabetsList
+                return False
+
+    def _get_sec(self,s):
+        l = s.split(':')
+        return int(l[0]) * 3600 + int(l[1]) * 60 + int(l[2])
+
+    def _findCorrespondingAlphabetForGivenTime(self,timeInSeconds,timeFrameToAlphabetList):
+        for key in timeFrameToAlphabetList:
+            intKey=int(key)
+            val=timeFrameToAlphabetList[key]
+            intTimeInSeconds=int(timeInSeconds)
+            if intKey > intTimeInSeconds:
+                return val
+
+
+
+    def _mapTimePeriodsToAlphabets(self,timeFrameInMin,profileAlphabetArray,startTime,EndTime):
+
+        startTimeInSeconds=self._get_sec(startTime)
+        endTimeInSeconds=self._get_sec(EndTime)
+        num=startTimeInSeconds
+        timeFrameInSeconds=timeFrameInMin*60;
+        counter=0;
+
+        while num < endTimeInSeconds:
+            self.timeFrameToAlphabetList[num]=profileAlphabetArray[counter]
+            counter=counter+1
+            num=num+timeFrameInSeconds
+
+    def execute(self,price,timeInSeconds):
+        correcpondingAlphabet=self._findCorrespondingAlphabetForGivenTime(timeInSeconds,self.timeFrameToAlphabetList)
+        if self.doStoreOnlyInHeap==True:
+            self._isAlphabetAlreadyIncludedInTheTick(price,correcpondingAlphabet)
+
+
+if __name__ == '__main__':
+    obj=StoreTickData("nifty","9:15:00","15:30:00",["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T"],30,True)
+    obj.execute(121,3000)
+
+
+
+
+
+
+
+
+
+
+
--- auctionTheory/meta_config.js
+++ auctionTheory/meta_config.js
@@ -1,7 +1,7 @@
 {
-"start_time":"09:15",
-"end_time":"3:30",
+"start_time":"09:15:00",
+"end_time":"15:30:00",
 "default_profile_period_in_mins":"30",
-"store_data_in_db_for_every_tick"true,
-"order_of_profile_alphabets":"ABCDEFGHIJKLMNOPQRST"
+"store_data_in_db_for_every_tick":false,
+"order_of_profile_alphabets":["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T"]
 }
